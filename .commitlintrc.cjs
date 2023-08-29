333395142cbd459f9544312ae947e43670558581
ruijadom <ruijadomingues@gmail.com>
2023-08-29 20:42:01 +0100

    chore(comitlint): rm help key

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -3,8 +3,6 @@ const conventional = require("@commitlint/config-conventional")
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   plugins: ["commitlint-plugin-function-rules"],
-  helpUrl:
-    "https://github.com/nextui-org/nextui/blob/main/CONTRIBUTING.MD#commit-convention",
   rules: {
     ...conventional.rules,
     "type-enum": [
