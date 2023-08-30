77799d34baca1e147cded1f65f194940edd464d5
ruijadom <ruijadomingues@gmail.com>
2023-08-30 18:46:05 +0100

    refactor: add logo and move custom styles to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import logo from "@/assets/logo.svg";
 
 function App() {
-  return <div className=" bg-slate-500">test</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <div className="content">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
