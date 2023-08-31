585086f4f72344ec5bb57b7af2c1fbe656cab19b
ruijadom <ruijadomingues@gmail.com>
2023-08-31 09:28:01 +0100

    chore(tailwind): allow define css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,62 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
+  darkMode: ["class"],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        Poppins: ["Poppins, sans-serif"],
+        montserrat: ["Montserrat"],
       },
-      container: {
-        center: true,
-        padding: "1rem",
-        screens: {
-          lg: "1125px",
-          xl: "1125px",
-          "2xl": "1125px",
+      fontSize: {
+        xs: ["0.688rem", { lineHeight: "0.838rem" }], // font-size - 11px; line-height - 13.41px
+        sm: ["0.75rem", { lineHeight: "0.914rem" }], // front-size - 12px; line-height - 14.63px
+        xl: ["1.25rem", { lineHeight: "1.524rem" }], // font-size - 20px; line-height - 24.38px
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        bold: 700,
+      },
+      colors: {
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        lighter: {
+          DEFAULT: "hsl(var(--lighter))",
+          foreground: "hsl(var(--lighter-foreground))",
+        },
+        lightest: {
+          DEFAULT: "hsl(var(--lightest))",
+          foreground: "hsl(var(--lightest-foreground))",
+        },
+        darker: {
+          DEFAULT: "hsl(var(--darker))",
+          foreground: "hsl(var(--darker-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        focus: "0.5px 0.5px 2px 2px hsl(var(--darker))",
       },
     },
   },
-  plugins: [],
 };
+
+module.exports = config;
